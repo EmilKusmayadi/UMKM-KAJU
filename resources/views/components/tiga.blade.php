c01e03fd6985e8debef63f617f41e159daf9260c
favolia <daisszo1455@gmail.com>
2024-01-31 21:14:18 +0800

    fix tiga.blade.php

--- resources/views/components/tiga.blade.php
+++ resources/views/components/tiga.blade.php
@@ -1,43 +1,43 @@
-<div class="bg-green h-[110vh] md:[60vh] px-8 md:py-10 flex flex-col justify-center items-center gap-y-10">
+<div class="bg-green px-8 py-10 flex flex-col justify-center items-center gap-y-10">
     <div class="w-full flex justify-between items-center text-white">
         <h1 class="font-bold text-2xl">Penawaran Spesial</h1>
         <a href="#" class="uppercase border-b-[1px] border-white px-1 font-light">semua</a>
     </div>
 
-    <div class="w-full h-[60vh] max-h-fit grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-x-5">
+    <div class="w-full flex justify-center items-center gap-x-5">
 
-        <div class="bg-[#D9F2F9] px-5 py-8 rounded-xl w-full h-full flex flex-col justify-between relative mb-7 md:mb-0">
+        <div class="bg-[#D9F2F9] px-5 py-8 rounded-xl w-full h-[65vh] flex flex-col justify-between relative">
             <div class="text-old-gray flex flex-col gap-y-8">
                 <p class="font-bold text-black">Bundle</p>
-                <h1 class="text-2xl md:text-4xl font-semibold leading-8">Belanja lebih <br> hemat dan lebih <br>puas</h1>
-                <p class="text-xs md:text-lg">Mulai dari Rp.3000 per kg</p>
+                <h1 class="text-3xl font-semibold leading-8">Belanja lebih <br> hemat dan lebih <br>puas</h1>
+                <p>Mulai dari Rp.3000 per kg</p>
             </div>
 
-            <div class="">
+            <div class="-translate-y-5">
                 <a href="#">
-                    <button class="bg-green px-3 md:px-6 py-2 text-white font-light rounded-lg text-sm">Beli Sekarang</button>
+                    <button class="bg-green px-6 py-2 text-white font-light rounded-lg text-sm">Beli Sekarang</button>
                 </a>
             </div>
 
-            <div class="w-44 md:w-80 self-end absolute bottom-10 md:bottom-0">
+            <div class="w-80 self-end absolute bottom-0">
                 <img src="{{ asset('image/organic-food-juice-vegetable-fruit-basket-vegetable-png-photos-6d5e358490722e7b06e5f983ba54fbb0.png') }}" alt="">
             </div>
         </div>
 
-        <div class="bg-[#D8EBE5] px-5 py-8 rounded-xl w-full h-full flex flex-col justify-between relative">
+        <div class="bg-[#D8EBE5] px-5 py-8 rounded-xl w-full h-[65vh] flex flex-col justify-between relative">
             <div class="text-old-gray flex flex-col gap-y-8">
                 <p class="font-bold text-black">Promo</p>
-                <h1 class="text-2xl md:text-4xl font-semibold leading-8">Penawaran<br> Menarik</h1>
-                <p class="text-xs mb-3 md:text-lg">Info lebih lanjut</p>
+                <h1 class="text-3xl font-semibold leading-8">Penawaran<br> Menarik</h1>
+                <p>Info lebih lanjut</p>
             </div>
 
-            <div class="">
+            <div class="-translate-y-5">
                 <a href="#">
-                    <button class="bg-green px-3 md:px-6 py-2 text-white font-light rounded-lg text-sm">Cek Sekarang</button>
+                    <button class="bg-green px-6 py-2 text-white font-light rounded-lg text-sm">Cek Sekarang</button>
                 </a>
             </div>
 
-            <div class="w-44 sm:w-55 md:w-80 self-end absolute bottom-0">
+            <div class="w-80 self-end absolute bottom-0">
                 <img src="{{ asset('image/fruit-supermarket-vegetable-auglis-supermarket-shelf-water-aaf86a1d356a5514c189d7f7ad03eea1.png') }}" alt="">
             </div>
         </div>
